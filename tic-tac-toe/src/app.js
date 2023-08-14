8944830b9ced8dff7c20a1e4922c5a256266a8f6
dalilahonic <dalilahonic1@gmail.com>
2023-08-14 23:06:16 +0200

    .

--- tic-tac-toe/src/app.js
+++ tic-tac-toe/src/app.js
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+
+// function Square({ value }) {
 function Square() {
-  return <button className="square">1</button>;
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    // console.log('clicked');
+    setValue('X');
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
+    //  we use fragments (<> and </>) to wrap multiple adjacent JSX elements like this:
     <>
       <div className="board">
         <div className="board-row">
+          {/* <Square value="1" /> */}
           <Square />
           <Square />
           <Square />
