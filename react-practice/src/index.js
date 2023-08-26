71aa4d74c09cdd427142df693881a91f279c18b9
dalilahonic <dalilahonic1@gmail.com>
2023-08-26 14:03:13 +0200

    .

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
@@ -13,4 +12,3 @@ root.render(
   </React.StrictMode>
 );
 
-// reportWebVitals();
