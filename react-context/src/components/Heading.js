c54d7a197758aba8b5ce8218e8ab20c834b62a4e
dalilahonic <dalilahonic1@gmail.com>
2023-09-24 13:47:28 +0200

    .

---
+++ react-context/src/components/Heading.js
@@ -0,0 +1,38 @@
+import { useContext } from 'react';
+import {
+  MyContext,
+  headingToggleFunction,
+} from '../context/MyContext';
+import { headingContext } from '../context/MyContext';
+
+export default function Heading({ headingClass }) {
+  const { timesChanged } = useContext(MyContext);
+
+  const [headingStyle, setHeadingStyle] =
+    useContext(headingContext);
+
+  // const headingToggleFun = useContext(
+  //   headingToggleFunction
+  // );
+
+  // console.log(headingStyle, setHeadingStyle);
+
+  function handleOnClick() {
+    setHeadingStyle((prev) => !prev);
+    // headingToggleFun();
+  }
+
+  return (
+    <>
+      <h1
+        className={`heading ${headingClass} ${
+          headingStyle ? 'gill-sans' : 'times-new-roman'
+        }`}
+      >
+        heading
+      </h1>
+      <p> Heading changed: {timesChanged} times</p>
+      <button onClick={handleOnClick}>Change style</button>
+    </>
+  );
+}
