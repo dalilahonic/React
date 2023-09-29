fb264718b72d85c7eed7110d52d838987a3be473
dalilahonic <dalilahonic1@gmail.com>
2023-09-29 19:27:18 +0200

    .

---
+++ custom-hooks/src/hooks/use-counter.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+function useCounter(plus = true) {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (plus) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return counter;
+}
+
+export default useCounter;
