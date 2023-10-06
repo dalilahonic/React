78b56e5225a563717f299f40fba90296b4d55904
dalilahonic <dalilahonic1@gmail.com>
2023-10-06 19:06:58 +0200

    .

---
+++ forms-and-user-input/src/hooks/use-input.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+function useInput(validateValue) {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  function reset() {
+    setEnteredValue('');
+    setIsTouched(false);
+  }
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset
+  };
+}
+export default useInput;
