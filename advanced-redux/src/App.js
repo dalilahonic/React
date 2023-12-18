cea73553f78ac2dc3c9b7111e6ce55f9c727af9d
dalilahonic <dalilahonic1@gmail.com>
2023-12-18 17:59:00 +0100

    .

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -1,18 +1,81 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
+
   const cartIsVisible = useSelector(
     (state) => state.ui.cartIsVisible
   );
-  console.log(cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector(
+    (state) => state.ui.notification
+  );
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    // async function sendCartData() {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'pending',
+    //       title: 'Sending...',
+    //       message: 'Sending cart data',
+    //     })
+    //   );
+    //   const response = await fetch(
+    //     'https://react-10d3f-default-rtdb.firebaseio.com/cart.json',
+    //     { method: 'PUT', body: JSON.stringify(cart) }
+    //   );
+    //   if (!response.ok) {
+    //     throw new Error('Sending cart data failed');
+    //   }
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'success',
+    //       title: 'Sucess!',
+    //       message: 'Sent cart data successfully!',
+    //     })
+    //   );
+    // }
+    // if (isInitial) {
+    //   isInitial = false;
+    //   return;
+    // }
+    // sendCartData().catch((err) => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'error',
+    //       title: 'Error!',
+    //       message: 'Sending cart data failed!',
+    //     })
+    //   );
+    // });
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
