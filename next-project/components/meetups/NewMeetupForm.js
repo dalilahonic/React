b745c3d26e6d2dfde83d4a9d1be6fe2e14c302c2
dalilahonic <dalilahonic1@gmail.com>
2024-01-01 23:11:00 +0100

    .

--- next-project/components/meetups/NewMeetupForm.js
+++ next-project/components/meetups/NewMeetupForm.js
@@ -15,7 +15,8 @@ function NewMeetupForm(props) {
     const enteredTitle = titleInputRef.current.value;
     const enteredImage = imageInputRef.current.value;
     const enteredAddress = addressInputRef.current.value;
-    const enteredDescription = descriptionInputRef.current.value;
+    const enteredDescription =
+      descriptionInputRef.current.value;
 
     const meetupData = {
       title: enteredTitle,
@@ -29,18 +30,36 @@ function NewMeetupForm(props) {
 
   return (
     <Card>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <form
+        className={classes.form}
+        onSubmit={submitHandler}
+      >
         <div className={classes.control}>
           <label htmlFor='title'>Meetup Title</label>
-          <input type='text' required id='title' ref={titleInputRef} />
+          <input
+            type='text'
+            required
+            id='title'
+            ref={titleInputRef}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor='image'>Meetup Image</label>
-          <input type='url' required id='image' ref={imageInputRef} />
+          <input
+            type='url'
+            required
+            id='image'
+            ref={imageInputRef}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor='address'>Address</label>
-          <input type='text' required id='address' ref={addressInputRef} />
+          <input
+            type='text'
+            required
+            id='address'
+            ref={addressInputRef}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor='description'>Description</label>
